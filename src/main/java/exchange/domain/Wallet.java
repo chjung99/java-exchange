4043ac3f3feced2cb839b2e7c8fbbd5bd8b02bda
chjung99 <cksgh1168@gmail.com>
2025-11-24 21:36:39 +0900

    refactor(wallet): 추상 클래스 제거

--- src/main/java/exchange/domain/Wallet.java
+++ src/main/java/exchange/domain/Wallet.java
@@ -1,8 +1,25 @@
 package exchange.domain;
 
-public class Wallet extends AbstractWallet{
+public class Wallet {
+    private final String userId;
+    private final WalletType type;
+    private double balance;
 
     public Wallet(String userId, WalletType type, double balance) {
-        super(userId, type, balance);
+        this.userId = userId;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    public synchronized boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized void deposit(double amount) {
+        balance += amount;
     }
 }
